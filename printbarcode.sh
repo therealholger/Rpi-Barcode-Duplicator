2789a456458c5dc67b3c1550184aaa90eaa12ad9
justaBernd <35521605+therealholger@users.noreply.github.com>
2021-09-04 09:57:01 +0200

    Update printbarcode.sh
    a missing ^ adds FS just to the barcode  and not to the human readable part

--- printbarcode.sh
+++ printbarcode.sh
@@ -3,7 +3,7 @@ do
   read bc 
   zpl1="^XA~SD30^LT18^FO230,4^A0N,50,55^FD"
   zpl2="^FS^BY2,2,50^FO220,55^BCN,50,N,N,N,N^FD"
-  zpl3="FS^XZ"
+  zpl3="^FS^XZ"
   fullzpl=$zpl1$bc$zpl2$bc$zpl3 #This inserts the variable bc in two places in the script, once for the custom interpretation line and once for the Code 128 barcode.  See the README.md file for details.
   echo $fullzpl
   echo $fullzpl > /tmp/bc.txt
